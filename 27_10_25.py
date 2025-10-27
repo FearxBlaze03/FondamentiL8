adcd5115d71a2f80fdf03dc8b0941258bcc8abe8
FearxBlaze03 <NVRFRC03T44G273Q@studium.unict.it>
2025-10-27 18:32:07 +0100

    Lezione 1 - 27/10/2025 - Variabili & I/0

--- 27_10_25.py
+++ 27_10_25.py
@@ -1,1 +1,56 @@
-print("Hello,World!")
+# # PRIMO PROGRAMMA IN PYTHON - CALCOLATRICE
+#
+# print("=== CALCOLATRICE ===")
+# print()
+#
+# # VARIABILI DALL'UTENTE
+#
+# a = float(input("Inserisci Primo Numero: "))
+# print(a)
+#
+# b = float(input("Inserisci Secondo Numero: "))
+# print(b)
+#
+# # OPERAZIONI DA SVOLGERE
+#
+# somma = a + b
+# sottr = a - b
+# molt = a * b
+# divisione = a / b
+#
+# # ARROTONDAMENTO DI UN NUMERO
+# divisione = round(divisione, 2)
+#
+# print(f"Somma: {somma}")
+# print(f"Sottrazione: {sottr}")
+# print(f"Moltiplicazione: {molt}")
+# print(
+#     f"Divisione: {divisione}")  # QUALORA UTILIZIAMO LA F-STRING, IL CONTENUTO DEL NOSTRO VALORE NON CAMBIERA', MENTRE SE UTILIZIAMO IL ROUND, LA VOSTRA VARIABILE VERRA' SOVRASCRITTA
+#
+#
+# -----------------------------------------------------------------------
+
+# CONVERTIRE TEMPERATURE DA FAHRENHEIT A CELSIUS
+
+# print("=== CONVERTITORE TEMPERATURA ===")
+# print()
+#
+# # VARIABILI DALL'UTENTE
+# gradi = float(input("Inserisci Temperatura In Fahrenheit: "))
+# print(gradi)
+#
+# celsius = (gradi - 32) * 0.555
+# print(f"Celsius: {celsius}")
+
+# -----------------------------------------------------------------------
+
+# # CALCOLARE L'AREA E IL PERIMETRO DI UN RETTANGOLO
+# base = float(input("Inserisci Base: "))
+# altezza = float(input("Inserisci Altezza: "))
+#
+# perimetro = (base + altezza) * 2
+# area = (base * altezza) / 2
+#
+# print(f"Perimetro: {perimetro}")
+# print(f"Area: {area}")
+#
