3ad980b879c696c9735f7a423b15a6b091eb2b00
FearxBlaze03 <NVRFRC03T44G273Q@studium.unict.it>
2025-11-02 11:39:22 +0100

    02 / 11 / 2025 - Svolti Esercizi Livello Base

--- While/Esercizi_Livello_Base.py
+++ While/Esercizi_Livello_Base.py
@@ -1,7 +1,7 @@
 # Chiedi all'utente di inserire una password. Continua a Chiedere Finchè la password inserita non coincide con quella corretta. 
 # Usa While e If per controllare la correttezza.
 
-password_corretta = "python123"
+""" password_corretta = "python123"
 
 password = input("Inserisci Password: ")
 
@@ -9,5 +9,7 @@ while password != password_corretta:
     print("Password Errata. Riprova.")
     password = input("Inserisci Password: ")
 
-print("Accesso Consentito.") 
+print("Accesso Consentito.")  """
     
+# ====================================================
+
