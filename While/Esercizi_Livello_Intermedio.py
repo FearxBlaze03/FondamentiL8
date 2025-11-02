81d1a44a6d0279f30e69289c668a749e38e8207f
FearxBlaze03 <NVRFRC03T44G273Q@studium.unict.it>
2025-11-02 12:30:27 +0100

    02 / 11 / 2025 - Svolti Esercizi Livello Base, Intermedio

---
+++ While/Esercizi_Livello_Intermedio.py
@@ -0,0 +1,3 @@
+# Chiedi all'utente di Inserire un numero intero e usa un ciclo While per verificare se è un numero primo
+# (divisibile solo per 1 e per sè stesso). Se non è primo, chiedi un altro numero finché non ne inserisce uno che lo è
+
