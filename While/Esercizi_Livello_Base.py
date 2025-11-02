03d5b8f94ff1152b766f0f578ba0a111f627b0ae
FearxBlaze03 <NVRFRC03T44G273Q@studium.unict.it>
2025-11-02 12:27:33 +0100

    02 / 11 / 2025 - Svolti Esercizi Livello Base

--- While/Esercizi_Livello_Base.py
+++ While/Esercizi_Livello_Base.py
@@ -1,5 +1,4 @@
-# Chiedi all'utente di inserire una password. Continua a Chiedere Finchè la password inserita non coincide con quella corretta. 
-# Usa While e If per controllare la correttezza.
+# Chiedi all'utente di inserire una password. Continua a Chiedere Finchè la password inserita non coincide con quella corretta. Usa While e If per controllare la correttezza.
 
 """ password_corretta = "python123"
 
@@ -12,4 +11,39 @@ while password != password_corretta:
 print("Accesso Consentito.")  """
     
 # ====================================================
+
+# Fai Inserire Numeri all'utente finché non digita 0. Usa If per contare quanti numeri pari e quanti dispari sono stati inseriti, poi mostra il risultato.
+
+""" pari = 0
+dispari = 0
+
+numero = int(input("Inserisci Un Numero (0 Per Terminare): "))
+
+while numero != 0:
+    if numero % 2 == 0:
+        pari += 1
+    else:
+        dispari += 1
+    numero = int(input("Inserisci Un Numero (0 Per Terminare): "))
+
+print(f"Numeri Pari Inseriti: {pari}")
+print(f"Numeri Dispari Inseriti: {dispari}") """
+
+# ====================================================
+
+# L'utente inserisce una serie di numeri interi. Inserendo 0 si interrompe il ciclo. Alla Fine, stampa il numero più grande inserito.
+
+""" numero = int(input("Inserisci Un Numero Intero (0 Per Terminare): "))
+massimo = numero
+
+while numero != 0:
+    if numero > massimo:
+        massimo = numero
+    numero = int(input("Inserisci Un Numero Intero (0 Per Terminare): "))
+
+print(f"Il Numero Più Grande Inserito è: {massimo}") """
+
+# ====================================================
+
+
 
