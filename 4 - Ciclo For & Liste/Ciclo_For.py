05e67fce7632b3d93a4bdbcbe18338b8d25c6f0e
FearxBlaze03 <NVRFRC03T44G273Q@studium.unict.it>
2025-11-03 15:47:56 +0100

    Lezione 4 - 03/1/2025 - Ciclo For

---
+++ 4 - Ciclo For & Liste/Ciclo_For.py
@@ -0,0 +1,18 @@
+"""somma = 0
+numero = 0
+
+for numero in range(2, 21, 2):
+    somma += numero
+
+print(f"La Somma di Tutti i Numeri Pari da 1 a 2 è: {somma}")"""
+
+# =================================================
+
+# Stampare Tutte le Tabelline da 1 a 10
+
+"""for n in range(1, 11):
+    for j in range(1, 11):
+        print(f"{n} x {j} = {n * j}")
+    print("-"*10) """
+
+# =================================================
